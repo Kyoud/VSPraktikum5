69fb65b5d972b236621883fdbe11ee5efa868d64
Kyoud <hollenbeckjn@yahoo.de>
2017-01-10 09:02:45 +0100

    commit

--- gen-cpp/server.cpp
+++ gen-cpp/server.cpp
@@ -1,6 +1,7 @@
 //This part is for beeing a Server
 #include <iostream>
 #include <vector>
+#include <string>
 #include <thrift/protocol/TBinaryProtocol.h>
 #include <thrift/server/TSimpleServer.h>
 #include <thrift/transport/TServerSocket.h>
@@ -25,35 +26,39 @@ using boost::shared_ptr;
 class resourceserverHandler : virtual public resourceserverIf {
 private:
     std::vector<resourceserverClient> servers;
-
+    int clock = 0;
+    std::string name = "";
+    bool locktry =false;
 
 public:
 
-    resourceserverHandler() {
+    resourceserverHandler(int own_port) {
+	std::cout <<"connection of server "<<own_port<<std::endl;
+        name = "Server"+own_port;
         // Your initialization goes here
         //connect to resource server 9091 res1
-        for (int i = 9090; i < 9004; i++) {
-            if (this->= !i) { //achtung pseudocode 
+        for (int i = 9090; i < 9094; i++) {
+	std::cout<<i<<std::endl;
+            if (own_port != i) { 
+		std::cout << "connect to server with port: "<<i<<std::endl;
                 boost::shared_ptr<TSocket> socket(new TSocket("127.0.0.1", i));
                 boost::shared_ptr<TTransport> transport(new TBufferedTransport(socket));
                 boost::shared_ptr<TProtocol> protocol(new TBinaryProtocol(transport));
                 resourceserverClient res1(protocol);
                 servers.push_back(res1);
+
             }
         }
     }
 
     void lock() {
         //unlock();
-        if (servers.size == 0)
-            resourceserverHandler();
-
         // Your implementation goes here
-        for (int i = 0; i < servers.size; i++) {
+        for (int i = 0; i < servers.size(); i++) {
             try {
                 servers.at(i).getOutputProtocol()->getTransport()->open();
                 std::cout << "request server" << std::endl;
-                servers.at(i).request("test", 1);
+                servers.at(i).request(name, 1);
                 servers.at(i).getOutputProtocol()->getTransport()->close();
 
 
@@ -63,19 +68,22 @@ public:
         }
         printf("lock\n");
         sleep(5);
-        unlock();
+        
     }
 
     void unlock() {
         // Your implementation goes here
-        printf("unlock\n");
+        std::cout<<"unlock"<<std::endl;
     }
 
     void request(const std::string& name, const int64_t time) {
         // Your implementation goes here
 
-        printf("request\n");
-        //res1.ack();
+        std::cout<<"request for resource lock from server: " +name<<std::endl;
+	if (locktry == true){
+	std::cout<<"this server is also trying to lock the resource"<<std::endl;
+	}
+        
     }
 
     void ack(const std::string& name, const int64_t time) {
@@ -93,7 +101,7 @@ int main(int argc, char **argv) {
     std::cin>>port;
 
     //initialize server profile
-    shared_ptr<resourceserverHandler> handler(new resourceserverHandler());
+    shared_ptr<resourceserverHandler> handler(new resourceserverHandler(port));
     shared_ptr<TProcessor> processor(new resourceserverProcessor(handler));
     shared_ptr<TServerTransport> serverTransport(new TServerSocket(port));
     shared_ptr<TTransportFactory> transportFactory(new TBufferedTransportFactory());
@@ -102,8 +110,9 @@ int main(int argc, char **argv) {
 
 
     TSimpleServer server(processor, serverTransport, transportFactory, protocolFactory);
-
     server.serve();
     return 0;
+}
+
 }
 
